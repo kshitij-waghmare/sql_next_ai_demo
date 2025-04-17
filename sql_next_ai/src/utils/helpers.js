11981fb37aabd3b5e58165dc0c1ea99c4067ec0f
kshitij-waghmare <kshitij.waghmare@mastek.com>
2025-04-18 01:53:44 +0530

    helper updated

--- sql_next_ai/src/utils/helpers.js
+++ sql_next_ai/src/utils/helpers.js
@@ -24,25 +24,49 @@ export function smoothScrollToBottom(element, duration = 1000) {
   requestAnimationFrame(animateScroll);
 }
 
-  // Scroll function with header offset
-  export function smoothScrollToElement(container, targetElement, duration = 1000, offset = 0) {
-    const start = container.scrollTop;
-    const targetOffsetTop = targetElement.offsetTop - offset;
-    const change = targetOffsetTop - start;
-    const startTime = performance.now();
-  
-    function animateScroll(currentTime) {
-      const timeElapsed = currentTime - startTime;
-      const progress = Math.min(timeElapsed / duration, 1);
-      const ease = easeInOutCubic(progress);
-  
-      container.scrollTop = start + change * ease;
-  
-      if (progress < 1) {
-        requestAnimationFrame(animateScroll);
-      }
+// Scroll function with header offset
+export function smoothScrollToElement(
+  container,
+  targetElement,
+  duration = 1000,
+  offset = 0
+) {
+  const start = container.scrollTop;
+  const targetOffsetTop = targetElement.offsetTop - offset;
+  const change = targetOffsetTop - start;
+  const startTime = performance.now();
+
+  function animateScroll(currentTime) {
+    const timeElapsed = currentTime - startTime;
+    const progress = Math.min(timeElapsed / duration, 1);
+    const ease = easeInOutCubic(progress);
+
+    container.scrollTop = start + change * ease;
+
+    if (progress < 1) {
+      requestAnimationFrame(animateScroll);
     }
-  
-    requestAnimationFrame(animateScroll);
   }
-  
+
+  requestAnimationFrame(animateScroll);
+}
+
+export const sendDeleteRequest = async (userId, token, API_URL) => {
+  if (!token) {
+    throw new Error("Unauthorized. No token provided.");
+  }
+  const response = await fetch(`${API_URL}/user/delete-request`, {
+    method: "POST",
+    headers: {
+      Authorization: `${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userId }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to send delete request.");
+  }
+
+  return response;
+};
